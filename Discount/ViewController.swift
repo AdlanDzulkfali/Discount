6b61f140e98b2816b19e01673c0de10d0516f73d
Adstyle <mohammadadlan91@gmail.com>
2014-10-25 03:59:48 +0800

    Initial Commit

---
+++ Discount/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Discount
+//
+//  Created by Mohammad Adlan on 10/25/14.
+//  Copyright (c) 2014 Alan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
